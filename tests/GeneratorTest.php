a6e1d59b2872bd6652a8c089e3f0ae7444e1f8fc
moklidia <kitsunegakkou@gmail.com>
2019-07-03 16:23:54 +0400

    test refactor

--- tests/GeneratorTest.php
+++ tests/GeneratorTest.php
@@ -11,15 +11,7 @@ class GeneratorTest extends TestCase
     {
         $path1 = __DIR__ . "/fixtures/before.json";
         $path2 = __DIR__ . "/fixtures/after.json";
-        $result = <<<EOT
-        {
-            host: hexlet.io
-          + timeout: 20
-          - timeout: 50
-          - proxy: 123.234.53.22
-          + verbose: true
-        }
-        EOT;
+        $result = file_get_contents(__DIR__ . "/fixtures/expected/flatPrettyJson");
 
         $this->assertEquals($result, generateDiff($path1, $path2, 'pretty'));
     }
@@ -28,33 +20,7 @@ class GeneratorTest extends TestCase
     {
         $path1 = __DIR__ . "/fixtures/beforeNested.json";
         $path2 = __DIR__ . "/fixtures/afterNested.json";
-        $result = <<<EOT
-        {
-            common: {
-                setting1: Value 1
-              - setting2: 200
-                setting3: true
-              - setting6: {
-                    key: value
-                }
-              + setting4: blah blah
-              + setting5: {
-                    key5: value5
-                }
-            }
-            group1: {
-              + baz: bars
-              - baz: bas
-                foo: bar
-            }
-          - group2: {
-                abc: 12345
-            }
-          + group3: {
-                fee: 100500
-            }
-        }
-        EOT;
+        $result = file_get_contents(__DIR__ . "/fixtures/expected/nestedPrettyJson");
 
         $this->assertEquals($result, generateDiff($path1, $path2, 'pretty'));
     }
@@ -63,15 +29,7 @@ class GeneratorTest extends TestCase
     {
         $path1 = __DIR__ . "/fixtures/before.yaml";
         $path2 = __DIR__ . "/fixtures/after.yaml";
-        $result = <<<EOT
-        {
-            host: hexlet.io
-          + timeout: 20
-          - timeout: 50
-          - proxy: 123.234.53.22
-          + verbose: true
-        }
-        EOT;
+        $result = file_get_contents(__DIR__ . "/fixtures/expected/flatPrettyYaml");
 
         $this->assertEquals($result, generateDiff($path1, $path2, 'pretty'));
     }
@@ -80,11 +38,7 @@ class GeneratorTest extends TestCase
     {
         $path1 = __DIR__ . "/fixtures/before.json";
         $path2 = __DIR__ . "/fixtures/after.json";
-        $result = <<<EOT
-      Property 'timeout' was changed. From '50' to '20'
-      Property 'proxy' was removed
-      Property 'verbose' was added with value: 'true'
-      EOT;
+        $result = file_get_contents(__DIR__ . "/fixtures/expected/flatPlainJson");
 
         $this->assertEquals($result, generateDiff($path1, $path2, 'plain'));
     }
@@ -93,15 +47,7 @@ class GeneratorTest extends TestCase
     {
         $path1 = __DIR__ . "/fixtures/beforeNested.json";
         $path2 = __DIR__ . "/fixtures/afterNested.json";
-        $result = <<<EOT
-      Property 'common.setting2' was removed
-      Property 'common.setting6' was removed
-      Property 'common.setting4' was added with value: 'blah blah'
-      Property 'common.setting5' was added with value: 'complex value'
-      Property 'group1.baz' was changed. From 'bas' to 'bars'
-      Property 'group2' was removed
-      Property 'group3' was added with value: 'complex value'
-      EOT;
+        $result = file_get_contents(__DIR__ . "/fixtures/expected/nestedPlainJson");
 
         $this->assertEquals($result, generateDiff($path1, $path2, 'plain'));
     }
